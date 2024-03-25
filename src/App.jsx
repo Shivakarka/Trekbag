9eb7e2ffc4009ac0a977a5be7fbb37a32fa90914
Shivakarka <shivakarka@gmail.com>
2024-03-25 17:25:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import BackgroundHeading from './BackgroundHeading'
+import Footer from './Footer'
+
+function App() {
+
+
+  return (
+    <>
+     <BackgroundHeading/>
+     <main></main>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
